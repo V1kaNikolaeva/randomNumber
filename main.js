72ef096b4b3cebbb98db0abda105ea858747fe5b
V1kaNikolaeva <114816050+V1kaNikolaeva@users.noreply.github.com>
2023-10-02 00:10:51 +0300

    commit

--- main.js
+++ main.js
@@ -1,52 +1,34 @@
 'use strict'
 
-// let intervalId;
 const number = document.getElementById('outputNumber');
 const sNumber = document.getElementById('sNumber');
 const doNumber = document.getElementById('doNumber'); 
- 
+const myFavoriteButton = document.getElementById('start');
+const myFavoriteButton2 = document.getElementById('stop');
+const myFavoriteButton3 = document.getElementById('console');
+
+
 //Функция рандома 
-const main = (max, min, rand) => { 
+myFavoriteButton.onclick = function(max, min, rand) { 
     max = Number(doNumber.value); 
     min = Number(sNumber.value); 
     rand = min + Math.random() * (max + 1 - min); 
     number.innerText = Math.floor(rand); 
     return; 
 } 
- 
-//Запуск функции рандома 
-const start = () => { 
-    // intervalId = setInterval(main, 1000);
-    main();
-    console.log('Интервал запущен'); 
+
+myFavoriteButton2.onclick = function() {
+    sNumber.value = '';
+    doNumber.value = '';
 }
 
-// const stop = () => {
-//     clearInterval(intervalId);
-//     console.log('Интервал очищен');
-//     if (document.getElementById('start').addEventListener('click', start, { once: true })) {
-//         const myFavoriteButton2 = document.getElementById('start').addEventListener('click', start, { once: false });
-//     }
-// }
-
-const consoleClear = () => {
-    console.clear();
+myFavoriteButton3.onclick = function() {
     number.innerText = '0';
 }
 
-const stop = () => {
-    sNumber.value = '';
-    doNumber.value = '';
-    console.log('очищен')
-}
 
-const myFavoriteButton = document.getElementById('stop');
-myFavoriteButton.onclick = stop;
 
-const myFavoriteButton2 = document.getElementById('start').addEventListener('click', start, { once: false }); //поставить true для одноразовго нажатия
 
-const myFavoriteButton3 = document.getElementById('console');
-myFavoriteButton3.onclick = consoleClear;
 
 
 
